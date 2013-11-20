f5d05dbab6df1057aff7d4f9e9d8ca8fffb6b477
Rodolfo Giometti <giometti@linux.it>
2013-11-20 18:32:00 +0100

    target at91: add Cosino board support
    Signed-off-by: Rodolfo Giometti <giometti@linux.it>

---
+++ target/linux/at91/profiles/cosino.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2013 Rodolfo Giometti <giometti@linux.it>
+# Copyright (C) 2013 HCE Engineering
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+define Profile/COSINO_MEGA2560
+	NAME:=Cosino + Mega 2560
+	LINUX_VERSION:=3.10.0
+	NAND_BLOCKSIZE=2048:128k
+	PACKAGES:=
+endef
+
+define Profile/COSINO_MEGA2560/Description
+	Cosino with Mega 2560 Extension by HCE Engineering
+endef
+
+$(eval $(call Profile,COSINO_MEGA2560))
